f96a77e38c33a1ac63681aba01b69f180b390bcb
choiaw1991 <choiaw1991@gmail.com>
2021-09-06 11:41:55 -0700

    initial commit.

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Aetna Application Up and Running!!!'))
+app.listen(3000, () => console.log('Server ready'))
