0a9e1fe2c5725078146acb6105ba4f0159f47f6b
choiaw1991 <choiaw1991@gmail.com>
2021-09-06 11:04:02 -0700

    Add package-lock file and update app.js file name.

--- aetna_app.js
+++
@@ -1,6 +0,0 @@
-onst express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Aetna Application Up and Running!!!'))
-app.listen(3000, () => console.log('Server ready'))
-~                                                           
